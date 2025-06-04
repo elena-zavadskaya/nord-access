a23f3b7b4365ea12f10bce8829342979661df834
Pastukhova Polina <90262977+PastukhovaPolina@users.noreply.github.com>
2025-06-04 17:55:34 +0300

    Delete vue_tests directory

--- vue_tests/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'; // Импорт Vue
-import App from './App.vue'; // Импорт главного компонента приложения
-import './assets/global.css'; // Импорт общих стилей приложения
-
-// Создаём приложение и монтируем его на элемент с id="app"
-createApp(App).mount('#app');
