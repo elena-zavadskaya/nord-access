a50989f0e2a4d7f451b4ba32f3a4898b4276cc67
Pastukhova Polina <90262977+PastukhovaPolina@users.noreply.github.com>
2025-06-04 17:54:43 +0300

    Delete sitenord directory

--- sitenord/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>&copy; НОРД-КАПИТАЛ, 2025. Пользовательское соглашение и Политика обработки персональных данных.</p>
-  </footer>
-);
-
-export default Footer;
